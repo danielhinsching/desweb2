65b64e323ec6e760e115558a2d2555d00b71ceb4
danielhinsching <danielsantoshinsching@gmail.com>
2026-09-24 07:14:00 -0300

    feat(aula20): Django Admin para gerenciar produtos

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Produto
+
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nome", "preco")
+    search_fields = ("nome",)
+    ordering = ("id",)
